fadd065a9bd5159ff5bdd5244fb62e158b837e71
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-09-08 15:41:48 -0300

    primeiro commit

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,39 @@
-const num = //seu numero aqui
+// # Exercício 1
 
+// Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
+    
+// Faça isso:
+    
+// 1. Utilizando ifs aninhados
+// ```jsx
+//     if(expressao){
+//         if(expressao){
+
+//         }
+//     }
+// ```
+// 2. Utilizando um operador lógico para unir duas operações relacionais
+// ```jsx
+//     if(expressao && expressao){ //&& para E
+//     //expressao || expressao para OU     
+    
+//     }
+// ```
+
+//---------------------------------------------------------------------------------------
+
+const num = Number(prompt("Digite o número:"))
+
+// if(num % 2 === 0){
+//     console.log("O número é divisivel por 2")
+//     if(num % 3 === 0)
+//     console.log("O número é divisivel por 3")
+// } else {
+//     console.log("Seu número não é divisivel por 2 e nem por 3")
+// }
+
+if(num % 2 === 0 && num % 3 === 0){
+    console.log("Seu número é divisivel por 2 e 3")
+} else {
+    console.log("Seu número não é divisivel por 2 e nem por 3")
+}
